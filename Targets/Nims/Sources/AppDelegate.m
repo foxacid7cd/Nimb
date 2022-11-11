bb8a0bf97ff08b314816ca1ab626febfc7ccfdbd
Yevhenii Matviienko <foxacid@keemail.me>
2022-11-11 18:33:17 +0200

    Add all nims_ui events as blocks in shared var

--- Targets/Nims/Sources/AppDelegate.m
+++ Targets/Nims/Sources/AppDelegate.m
@@ -10,45 +10,153 @@
 
 #import <Cocoa/Cocoa.h>
 
+#define NS_STRING(arg) [[NSString alloc] initWithBytes:arg.data length:arg.size encoding:NSUTF8StringEncoding]
+
 @interface AppDelegate : NSObject <NSApplicationDelegate> {
-  xpc_connection_t active_connection;
+  NSWindow *window;
 }
 
 @end
 
 @implementation AppDelegate
 
-- (void)applicationDidBecomeActive:(NSNotification *)notification
-{
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+  self->window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 400, 300)
+                                          styleMask:NSWindowStyleMaskTitled
+                                            backing:NSBackingStoreBuffered
+                                              defer:true
+                                             screen:NULL];
+  [self->window orderFront:NULL];
+  
+  [self start_nvim];
+}
+
+- (void) start_nvim {
+  nvims_ui.mode_info_set = ^(_Bool enabled, void *cursor_styles) {
+    NSLog(@"nvims_ui.mode_info_set");
+  };
+  
+  nvims_ui.update_menu = ^() {
+    NSLog(@"nvims_ui.update_menu");
+  };
+  
+  nvims_ui.busy_start = ^() {
+    NSLog(@"nvims_ui.busy_start");
+  };
+  
+  nvims_ui.busy_stop = ^() {
+    NSLog(@"nvims_ui.busy_stop");
+  };
+  
+  nvims_ui.mouse_on = ^() {
+    NSLog(@"nvims_ui.mouse_on");
+  };
+  
+  nvims_ui.mouse_off = ^() {
+    NSLog(@"nvims_ui.mouse_off");
+  };
+  
+  nvims_ui.mode_change = ^(nvim_string_t mode, int64_t mode_idx) {
+    NSLog(@"nvims_ui.mode_change");
+  };
+  
+  nvims_ui.bell = ^() {
+    NSLog(@"nvims_ui.bell");
+  };
+  
+  nvims_ui.visual_bell = ^() {
+    NSLog(@"nvims_ui.visual_bell");
+  };
+  
+  nvims_ui.flush = ^() {
+    NSLog(@"nvims_ui.flush");
+  };
+  
+  nvims_ui.suspend = ^() {
+    NSLog(@"nvims_ui.suspend");
+  };
+  
+  nvims_ui.set_title = ^(nvim_string_t title) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+      id object = NS_STRING(title);
+      NSLog(@"nvims_ui.set_title %@", object);
+      [self->window setTitle:object];
+    });
+  };
+  
+  nvims_ui.set_icon = ^(nvim_string_t icon) {
+    NSLog(@"nvims_ui.set_icon %@", NS_STRING(icon));
+  };
+  
+  nvims_ui.screenshot = ^(nvim_string_t path) {
+    NSLog(@"nvims_ui.screenshot %@", NS_STRING(path));
+  };
+  
+  nvims_ui.option_set = ^(nvim_string_t name, void *value) {
+    NSLog(@"nvims_ui.option_set %@", NS_STRING(name));
+  };
+  
+  nvims_ui.stop = ^() {
+    NSLog(@"nvims_ui.stop");
+  };
+  
+  nvims_ui.default_colors_set = ^(int64_t rgb_fg, int64_t rgb_bg, int64_t rgb_sp, int64_t cterm_fg, int64_t cterm_bg) {
+    NSLog(@"nvims_ui.default_colors_set");
+  };
+  
+  nvims_ui.hl_attr_define = ^(int64_t id, nvim_hl_attrs_t rgb_attrs, nvim_hl_attrs_t cterm_attrs, void *info) {
+    NSLog(@"nvims_ui.hl_attr_define");
+  };
+  
+  nvims_ui.hl_group_set = ^(nvim_string_t name, int64_t id) {
+    NSLog(@"nvims_ui.hl_group_set %@", NS_STRING(name));
+  };
+  
+  nvims_ui.grid_resize = ^(int64_t grid, int64_t rows, int64_t cols) {
+    NSLog(@"nvims_ui.grid_resize %lli %lli %lli", grid, rows, cols);
+  };
+  
+  nvims_ui.grid_clear = ^(int64_t grid) {
+      NSLog(@"nvims_ui.grid_clear %lli", grid);
+  };
+  
+  nvims_ui.grid_cursor_goto = ^(int64_t grid, int64_t row, int64_t col) {
+      NSLog(@"nvims_ui.grid_cursor_goto %lli %lli %lli", grid, row, col);
+  };
+  
+  nvims_ui.grid_scroll = ^(int64_t grid, int64_t top, int64_t bot, int64_t left, int64_t right, int64_t rows, int64_t cols) {
+      NSLog(@"nvims_ui.grid_scroll");
+  };
+  
+  nvims_ui.raw_line = ^(int64_t grid, int64_t row, int64_t startcol, int64_t endcol, int64_t clearcol, int64_t clearattr, int64_t flags, const void * chunk, const int32_t * attrs) {
+      NSLog(@"nvims_ui.raw_line");
+  };
+  
+  nvims_ui.event = ^(char *name, void *args) {
+    NSLog(@"nvims_ui.event %s", name);
+  };
+  
+  nvims_ui.msg_set_pos = ^(int64_t grid, int64_t row, _Bool scrolled, nvim_string_t sep_char){
+    NSLog(@"nvims_ui.msg_set_pos");
+  };
+  
+  nvims_ui.win_viewport = ^(int64_t grid, nvim_handle_t win, int64_t topline, int64_t botline, int64_t curline, int64_t curcol, int64_t line_count) {
+    NSLog(@"nvims_ui.win_viewport");
+  };
+  
+  nvims_ui.wildmenu_show = ^(void *items) {
+    NSLog(@"nvims_ui.wildmenu_show");
+  };
+  
+  nvims_ui.wildmenu_select = ^(int64_t selected) {
+    NSLog(@"nvims_ui.wildmenu_select");
+  };
+  
+  nvims_ui.wildmenu_hide = ^() {
+    NSLog(@"nvims_ui.wildmenu_hide");
+  };
+  
   nvims_start();
-//  xpc_connection_t connection = xpc_connection_create("foxacid7cd.Nims.nvimd", NULL);
-//  self->active_connection = connection;
-//
-//  xpc_connection_set_event_handler(connection, ^(xpc_object_t object) {
-//    xpc_type_t type = xpc_get_type(object);
-//
-//    if (type == XPC_TYPE_ERROR) {
-//      const char *description = xpc_dictionary_get_string(object, XPC_ERROR_KEY_DESCRIPTION);
-//
-//      NSLog(@"XPC error: %s", description);
-//      return;
-//    }
-//
-//    int64_t message_type = xpc_dictionary_get_int64(object, NVIMD_MESSAGE_KEY_TYPE);
-//    switch (message_type) {
-//      case NvimdMessageTypeStart:
-//        NSLog(@"Started!");
-//
-//      default:
-//        break;
-//    }
-//  });
-//
-//  xpc_connection_activate(connection);
-//
-//  xpc_object_t message = xpc_dictionary_create_empty();
-//  xpc_dictionary_set_int64(message, "type", NvimdMessageTypeStart);
-//  xpc_connection_send_message(connection, message);
 }
 
 @end
--- Targets/nvims/include/nvims.h
+++ Targets/nvims/include/nvims.h
@@ -9,6 +9,78 @@
 #ifndef nvims_h
 #define nvims_h
 
+#include <stdio.h>
+
+typedef struct {
+  char *data;
+  size_t size;
+} nvim_string_t;
+
+typedef int64_t nvim_handle_t;
+
+typedef int32_t nvim_rgb_value_t;
+
+typedef enum : int16_t {
+  NvimHlAttrFlagsInverse = 0x01,
+  NvimHlAttrFlagsBold = 0x02,
+  NvimHlAttrFlagsItalic = 0x04,
+  NvimHlAttrFlagsUnderline = 0x08,
+  NvimHlAttrFlagsUndercurl = 0x10,
+  NvimHlAttrFlagsUnderdouble = 0x20,
+  NvimHlAttrFlagsUnderdotted = 0x40,
+  NvimHlAttrFlagsUnderdashed = 0x80,
+  NvimHlAttrFlagsStandout = 0x0100,
+  NvimHlAttrFlagsNoCombine = 0x0200,
+  NvimHlAttrFlagsStrikethrough = 0x0400,
+  NvimHlAttrFlagsBgIndexed = 0x0800,
+  NvimHlAttrFlagsFgIndexed = 0x1000,
+  NvimHlAttrFlagsDefault = 0x2000,
+  NvimHlAttrFlagsGlobal = 0x4000,
+  NvimHlAttrFlagsAnyUnderline = NvimHlAttrFlagsUnderline | NvimHlAttrFlagsUnderdouble | NvimHlAttrFlagsUndercurl | NvimHlAttrFlagsUnderdotted | NvimHlAttrFlagsUnderdashed,
+} nvim_hl_attr_flags_t;
+
+typedef struct {
+  nvim_hl_attr_flags_t rgb_ae_attr, cterm_ae_attr;
+  nvim_rgb_value_t rgb_fg_color, rgb_bg_color, rgb_sp_color;
+  int cterm_fg_color, cterm_bg_color;
+  int hl_blend;
+} nvim_hl_attrs_t;
+
+typedef struct {
+  void (^mode_info_set)(_Bool enabled, void *cursor_styles);
+  void (^update_menu)(void);
+  void (^busy_start)(void);
+  void (^busy_stop)(void);
+  void (^mouse_on)(void);
+  void (^mouse_off)(void);
+  void (^mode_change)(nvim_string_t mode, int64_t mode_idx);
+  void (^bell)(void);
+  void (^visual_bell)(void);
+  void (^flush)(void);
+  void (^suspend)(void);
+  void (^set_title)(nvim_string_t title);
+  void (^set_icon)(nvim_string_t icon);
+  void (^screenshot)(nvim_string_t path);
+  void (^option_set)(nvim_string_t name, void *value);
+  void (^stop)(void);
+  void (^default_colors_set)(int64_t rgb_fg, int64_t rgb_bg, int64_t rgb_sp, int64_t cterm_fg, int64_t cterm_bg);
+  void (^hl_attr_define)(int64_t id, nvim_hl_attrs_t rgb_attrs, nvim_hl_attrs_t cterm_attrs, void *info);
+  void (^hl_group_set)(nvim_string_t name, int64_t id);
+  void (^grid_resize)(int64_t grid, int64_t width, int64_t height);
+  void (^grid_clear)(int64_t grid);
+  void (^grid_cursor_goto)(int64_t grid, int64_t row, int64_t col);
+  void (^grid_scroll)(int64_t grid, int64_t top, int64_t bot, int64_t left, int64_t right, int64_t rows, int64_t cols);
+  void (^raw_line)(int64_t grid, int64_t row, int64_t startcol, int64_t endcol, int64_t clearcol, int64_t clearattr, int64_t flags, const void *chunk, const int32_t *attrs);
+  void (^event)(char *name, void *args);
+  void (^msg_set_pos)(int64_t grid, int64_t row, _Bool scrolled, nvim_string_t sep_char);
+  void (^win_viewport)(int64_t grid, nvim_handle_t win, int64_t topline, int64_t botline, int64_t curline, int64_t curcol, int64_t line_count);
+  void (^wildmenu_show)(void *items);
+  void (^wildmenu_select)(int64_t selected);
+  void (^wildmenu_hide)(void);
+} nvims_ui_t;
+
+nvims_ui_t nvims_ui;
+
 void nvims_start(void);
 
 #endif /* nvims_h */
