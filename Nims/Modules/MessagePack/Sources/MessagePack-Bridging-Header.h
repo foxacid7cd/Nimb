87a166b72aacbbe05b97f3d8d51ea425eba13632
Yevhenii Matviienko <foxacid@keemail.me>
2022-11-28 19:14:38 +0200

    Add a lot of CoreFoundation socket handling

---
+++ Nims/Modules/MessagePack/Sources/MessagePack-Bridging-Header.h
@@ -0,0 +1,2 @@
+
+#include "msgpack.h"
--- Nims/Sources/AppDelegate.swift
+++ Nims/Sources/AppDelegate.swift
@@ -1,7 +1,14 @@
+//
+//  AppDeletate.swift
+//  Nims
+//
+//  Created by Yevhenii Matviienko on 28.11.2022.
+//
 
 import Cocoa
 import NvimServiceAPI
 import OSLog
+import MessagePack
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -34,103 +41,124 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   
   private func startNvim(nvimService: NvimServiceProtocol) {
     let temporaryFileName = "\(UUID().uuidString).nvim"
-    let localSocketURL = FileManager.default.temporaryDirectory
-      .appending(component: temporaryFileName, directoryHint: .notDirectory)
+    
+    let localSocketURL = URL(fileURLWithPath: "/tmp", isDirectory: true)
+      .appendingPathComponent(temporaryFileName, isDirectory: false)
     
     let arguments = ["--listen", localSocketURL.relativePath]
     
     os_log("Starting nvim with arguments: \(arguments)")
     
     nvimService.startNvim(arguments: arguments) {
-      os_log("Started nvim")
-      
-      let callbackTypes: UInt = [
-        CFSocketCallBackType.dataCallBack,
-        CFSocketCallBackType.connectCallBack
-      ]
-        .map { $0.rawValue }
-        .reduce(0, |)
-      
-      let socketCallback: CFSocketCallBack = { socket, type, data, _, _ in
-        switch type {
-        case .connectCallBack:
-          os_log("Socket connect callback")
-          
-        case .dataCallBack:
-          os_log("socketCallback data \(CFDataGetLength(data))")
-          
-        default:
-          os_log("Socket unknown callback")
-        }
-      }
-      
-      let socket = CFSocketCreate(
-        nil,
-        PF_LOCAL,
-        SOCK_STREAM,
-        0,
-        callbackTypes,
-        socketCallback,
-        nil
-      )!
-      
-      let runLoopSource = CFSocketCreateRunLoopSource(nil, socket, 0)!
-      CFRunLoopAddSource(
-        RunLoop.main.getCFRunLoop(),
-        runLoopSource,
-        .defaultMode
-      )
-      
-      var address = sockaddr_un()
-      address.sun_family = sa_family_t(AF_UNIX)
-      
-      withUnsafeMutablePointer(to: &address.sun_path.0) { pointer in
-        localSocketURL.relativePath
-          .utf8CString
-          .withUnsafeBytes {
-            let pathPointer = $0
-              .assumingMemoryBound(to: CChar.self)
-              .baseAddress!
+      let socketThread = Thread {
+        os_log("Started nvim")
+        
+        let callbackTypes: UInt = [
+          CFSocketCallBackType.dataCallBack,
+          CFSocketCallBackType.writeCallBack,
+          CFSocketCallBackType.connectCallBack
+        ]
+          .map { $0.rawValue }
+          .reduce(0, |)
+        
+        let socketCallback: CFSocketCallBack = { socket, type, cfData, _, _ in
+          switch type {
+          case .connectCallBack:
+            os_log("Socket connect callback")
             
-            let pathLength = strlen(pathPointer)
-            address.sun_len = UInt8(pathLength)
+          case .dataCallBack:
+            let data = cfData! as Data
+            message_unpack(data: data)
             
-            strncpy(
-              pointer,
-              pathPointer,
-              pathLength
-            )
+          case .writeCallBack:
+            os_log("Socket write callback")
+            
+            let data = message_pack()
+            CFSocketSendData(socket, nil, data as CFData, 0)
+            
+            os_log("Sent \(data.count) bytes to socket")
+            
+          default:
+            os_log("Socket unknown callback")
           }
-      }
-      
-      let result = withUnsafeBytes(of: &address) { bufferPointer in
-        let data = CFDataCreate(
+        }
+        
+        let socket = CFSocketCreate(
           nil,
-          bufferPointer
-            .assumingMemoryBound(to: UInt8.self)
-            .baseAddress,
-          bufferPointer.count
+          PF_LOCAL,
+          SOCK_STREAM,
+          0,
+          callbackTypes,
+          socketCallback,
+          nil
+        )!
+        self.socket = socket
+        
+        let runLoopSource = CFSocketCreateRunLoopSource(nil, socket, 0)!
+        CFRunLoopAddSource(
+          RunLoop.current.getCFRunLoop(),
+          runLoopSource,
+          .defaultMode
         )
         
-        return CFSocketConnectToAddress(socket, data, 0)
+        RunLoop.current.schedule {
+          var address = sockaddr_un()
+          address.sun_family = sa_family_t(AF_UNIX)
+          
+          withUnsafeMutablePointer(to: &address.sun_path.0) { pointer in
+            localSocketURL.relativePath
+              .utf8CString
+              .withUnsafeBytes {
+                let pathPointer = $0
+                  .assumingMemoryBound(to: CChar.self)
+                  .baseAddress!
+                
+                let pathLength = strlen(pathPointer)
+                address.sun_len = UInt8(pathLength) + 1
+                
+                strncpy(
+                  pointer,
+                  pathPointer,
+                  pathLength
+                )
+              }
+          }
+          
+          let data = withUnsafeBytes(of: &address) { bufferPointer in
+            return Data(bytes: bufferPointer.baseAddress!, count: bufferPointer.count)
+          }
+          let cfData = data as CFData
+          
+          let result = CFSocketConnectToAddress(socket, cfData, 0)
+          
+          switch result {
+          case .success:
+            os_log("Socket connection success")
+            
+          case .error:
+            os_log("Socket connection error")
+            
+          case .timeout:
+            os_log("Socket connection timeout")
+            
+          default:
+            os_log("Socket connection unknown")
+          }
+        }
+        
+        self.socketRunLoop = RunLoop.current
+        RunLoop.current.run()
       }
+      self.socketThread = socketThread
       
-      switch result {
-      case .success:
-        os_log("Socket connection success")
-        
-      case .error:
-        os_log("Socket connection error")
-        
-      case .timeout:
-        os_log("Socket connection timeout")
-        
-      default:
-        os_log("Socket connection unknown")
-      }
+      socketThread.name = "\(Bundle.main.bundleIdentifier!).CFSocketThread"
+      socketThread.start()
     }
   }
   
   private var connection: NSXPCConnection?
   private var nvimService: NvimServiceProtocol?
+  private var socketThread: Thread?
+  private var socketRunLoop: RunLoop?
+  private var socket: CFSocket?
 }
