58395c4e7fdf572db5f09dbde7bc8e7ad8afdab7
Yevhenii Matviienko <foxacid@keemail.me>
2023-12-28 21:02:14 +0200

    Make runtime buf_text_for_copy use pcall

--- NeovimRuntime/src/lua/nims-gui/init.lua
+++ NeovimRuntime/src/lua/nims-gui/init.lua
@@ -9,13 +9,22 @@ function M.buf_text_for_copy()
 
   local a_orig = vim.fn.getreg("a")
   local mode = vim.fn.mode()
-  if mode ~= "v" and mode ~= "V" then
-    vim.cmd([[normal! V]])
-  end
-  vim.cmd([[silent! normal! "aygv]])
+
+  local status, error = pcall(function()
+    if mode ~= "v" and mode ~= "V" then
+      vim.cmd([[normal! V]])
+    end
+    vim.cmd([[normal! "aygv]])
+  end)
+
   local text = vim.fn.getreg("a")
   vim.fn.setreg("a", a_orig)
-  return r.success(text)
+
+  if not status and error then
+    return r.failure(error)
+  else
+    return r.success(text)
+  end
 end
 
 function M.edit(path)
