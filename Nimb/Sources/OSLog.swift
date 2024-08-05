3445ee8fb8e411a3450ecc579e6a3ab23f8a83ab
Yevhenii Matviienko <foxacid@keemail.me>
2024-08-06 02:05:37 +0300

    Add signposts for Actions.ApplyUIEvents

---
+++ Nimb/Sources/OSLog.swift
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+
+import OSLog
+
+let log = Logger()
+let signposter = OSSignposter(logger: log)
--- Nimb/Sources/log.swift
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-import OSLog
-
-let log = Logger(subsystem: "foxacid7cd", category: "Nimb")
