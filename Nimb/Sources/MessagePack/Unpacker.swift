de08805cac635920809bc451c7308c8f9b62ebf5
Yevhenii Matviienko <foxacid@keemail.me>
2024-08-06 00:31:46 +0300

    Remove new update func from Unpacker

--- Nimb/Sources/MessagePack/Unpacker.swift
+++ Nimb/Sources/MessagePack/Unpacker.swift
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 import AsyncAlgorithms
+import CustomDump
 import Foundation
 
 public class Unpacker {
@@ -20,7 +21,7 @@ public class Unpacker {
     }
 
     data.withUnsafeBytes { pointer in
-      msgpack_unpacker_buffer(&self.mpac)!
+      msgpack_unpacker_buffer(&mpac)!
         .initialize(
           from: pointer.baseAddress!.assumingMemoryBound(to: CChar.self),
           count: pointer.count
@@ -29,72 +30,29 @@ public class Unpacker {
     msgpack_unpacker_buffer_consumed(&mpac, data.count)
 
     var accumulator = [Value]()
+    while true {
+      let result = msgpack_unpacker_next(&mpac, &unpacked)
 
-    var result = msgpack_unpacker_next(&mpac, &unpacked)
-    var isCancelled = false
-
-    while !isCancelled {
       switch result {
-      case MSGPACK_UNPACK_SUCCESS:
-        let value = Value(unpacked.data)
-        accumulator.append(value)
+      case MSGPACK_UNPACK_EXTRA_BYTES,
+           MSGPACK_UNPACK_SUCCESS:
+        accumulator.append(.init(unpacked.data))
 
-      case MSGPACK_UNPACK_CONTINUE: isCancelled = true
+      case MSGPACK_UNPACK_CONTINUE:
+        return accumulator
 
       case MSGPACK_UNPACK_PARSE_ERROR:
-        throw Failure("Msgpack unpacking parse error")
+        throw Failure("MSGPACK_UNPACK_PARSE_ERROR")
+
+      case MSGPACK_UNPACK_NOMEM_ERROR:
+        throw Failure("MSGPACK_UNPACK_NOMEM_ERROR")
 
       default:
         throw Failure("Invalid msgpack unpacking result \(result)")
       }
-
-      result = msgpack_unpacker_next(&mpac, &unpacked)
-    }
-
-    return accumulator
-  }
-
-  public func _unpack(
-    source: UnsafeRawPointer,
-    bytesCount: Int
-  ) throws
-    -> (value: Value?, bytesUsed: Int)
-  {
-    let requiredCapacity = bufferedBytesCount + bytesCount
-    if msgpack_unpacker_buffer_capacity(&mpac) < requiredCapacity {
-      msgpack_unpacker_reserve_buffer(&mpac, requiredCapacity)
-    }
-
-    UnsafeMutableRawPointer(msgpack_unpacker_buffer(&mpac))!
-      .copyMemory(from: source, byteCount: bytesCount)
-
-    msgpack_unpacker_buffer_consumed(&mpac, bytesCount)
-
-    var p_bytes = 0
-    let result = msgpack_unpacker_next_with_size(&mpac, &unpacked, &p_bytes)
-
-    switch result {
-    case MSGPACK_UNPACK_EXTRA_BYTES,
-         MSGPACK_UNPACK_SUCCESS:
-      bufferedBytesCount = 0
-      return (value: .init(unpacked.data), bytesUsed: p_bytes)
-
-    case MSGPACK_UNPACK_CONTINUE:
-      bufferedBytesCount += p_bytes
-      return (value: nil, bytesUsed: p_bytes)
-
-    case MSGPACK_UNPACK_PARSE_ERROR:
-      throw Failure("MSGPACK_UNPACK_PARSE_ERROR")
-
-    case MSGPACK_UNPACK_NOMEM_ERROR:
-      throw Failure("MSGPACK_UNPACK_PARSE_ERROR")
-
-    default:
-      throw Failure("Unexpected result from msgpack_unpacker_next_with_size")
     }
   }
 
   private var mpac = msgpack_unpacker()
   private var unpacked = msgpack_unpacked()
-  private var bufferedBytesCount = 0
 }
--- speed-tuner/SpeedTuner.swift
+++ speed-tuner/SpeedTuner.swift
@@ -25,59 +25,11 @@ struct SpeedTuner: AsyncParsableCommand {
     )
     let data = try Data(contentsOf: dataFileURL)
 
-    let buffer = UnsafeMutableRawBufferPointer.allocate(
-      byteCount: 1024 * 1024,
-      alignment: 1024 * 16
-    )
-    data.copyBytes(to: buffer)
-    if data.count > buffer.count {
-      throw NoMemoryError()
-    }
-
     let unpacker = Unpacker()
-    var isCancelled = false
-    var source = buffer.baseAddress!
-    var bytesCount = data.count
-    while !isCancelled {
-      let (value, bytesUsed) = try unpacker._unpack(
-        source: buffer.baseAddress!,
-        bytesCount: data.count
-      )
-      bytesCount -= bytesUsed
-      if bytesCount <= 0 {
-        assert(bytesCount == 0, "Used bytes inconsistency")
-        isCancelled = true
-      } else {
-        source = source.advanced(by: bytesUsed)
-      }
-      if let value {
-        customDump(value)
-      }
+    let duration = try ContinuousClock().measure {
+      let values = try unpacker.unpack(data)
+      print("values: \(values.count)")
     }
-
-//    let fileHandle = try FileHandle(forReadingFrom: dataFileURL)
-//
-//    let outputFileURL = FileManager.default.temporaryDirectory.appending(path:
-//    "\(UUID().uuidString).txt")
-//    FileManager.default.createFile(atPath: outputFileURL.path(), contents: nil)
-//    let outputFileHandle = try FileHandle(forWritingTo: outputFileURL)
-//
-//    let unpacker = Unpacker()
-//    for dataBatch in [data] {
-//      let unpacked = try unpacker.unpack(dataBatch)
-//
-//      var string = ""
-//      customDump(unpacked, to: &string)
-//
-//      let data = string.data(using: .utf8)!
-//      try outputFileHandle.write(contentsOf: data)
-//    }
-//
-//    try outputFileHandle.synchronize()
-//    try outputFileHandle.close()
-//
-//    NSWorkspace.shared.open(outputFileURL)
+    print("duration \(duration)")
   }
 }
-
-struct NoMemoryError: Error { }
