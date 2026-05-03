fdadbe0f89bca1260fa490e05103a846e2761225
Yevhenii Matviienko <foxacid@keemail.me>
2026-05-03 04:21:51 +0300

    Fix drawing in GridLayer

--- Nimb/Sources/Main/GridLayer.swift
+++ Nimb/Sources/Main/GridLayer.swift
@@ -4,10 +4,10 @@ import Algorithms
 import Collections
 import ConcurrencyExtras
 import CustomDump
-import QuartzCore
+import AppKit
 import Queue
+import Synchronization
 
-@MainActor
 public class GridLayer: CALayer, Rendering, @unchecked Sendable {
   private struct DrawSnapshot {
     let grid: Grid
@@ -20,6 +20,22 @@ public class GridLayer: CALayer, Rendering, @unchecked Sendable {
 
   private let gridID: Grid.ID
   private let store: Store
+  private nonisolated let isolatedRenderContext = Mutex<RenderContext?>(nil)
+
+  @MainActor
+  public var isRendered: Bool {
+    isolatedRenderContext.withLock { $0 != nil }
+  }
+
+  @MainActor
+  public var renderContext: RenderContext {
+    isolatedRenderContext.withLock { $0! }
+  }
+
+  @MainActor
+  public func update(renderContext: RenderContext) {
+    isolatedRenderContext.withLock { $0 = renderContext }
+  }
 
   @MainActor
   public var grid: Grid? {
@@ -65,68 +81,74 @@ public class GridLayer: CALayer, Rendering, @unchecked Sendable {
   }
 
   override public func draw(in ctx: CGContext) {
-//    guard let snapshot = MainActor.assumeIsolated({ () -> DrawSnapshot? in
-//      guard isRendered, let grid, let upsideDownTransform else {
-//        return nil
-//      }
-//
-//      return DrawSnapshot(
-//        grid: grid,
-//        upsideDownTransform: upsideDownTransform,
-//        font: state.font,
-//        appearance: state.appearance,
-//        cursorBlinkingPhase: state.cursorBlinkingPhase,
-//        isMouseUserInteractionEnabled: state.isMouseUserInteractionEnabled
-//      )
-//    }) else {
-//      return
-//    }
-//
-//    ctx.saveGState()
-//    defer { ctx.restoreGState() }
-//
-//    let boundingRect = IntegerRectangle(
-//      frame: ctx.boundingBoxOfClipPath.applying(snapshot.upsideDownTransform),
-//      cellSize: snapshot.font.cellSize
-//    )
-//
-//    ctx.setAllowsAntialiasing(false)
-//    ctx.setAllowsFontSmoothing(false)
-//    ctx.setShouldAntialias(false)
-//    ctx.setShouldSmoothFonts(false)
-//    snapshot.grid.drawRuns.drawBackground(
-//      to: ctx,
-//      boundingRect: boundingRect,
-//      font: snapshot.font,
-//      appearance: snapshot.appearance,
-//      upsideDownTransform: snapshot.upsideDownTransform
-//    )
-//
-//    ctx.setAllowsAntialiasing(true)
-//    ctx.setAllowsFontSmoothing(true)
-//    ctx.setShouldAntialias(true)
-//    ctx.setShouldSmoothFonts(true)
-//    snapshot.grid.drawRuns.drawForeground(
-//      to: ctx,
-//      boundingRect: boundingRect,
-//      font: snapshot.font,
-//      appearance: snapshot.appearance,
-//      upsideDownTransform: snapshot.upsideDownTransform
-//    )
-//
-//    if
-//      snapshot.cursorBlinkingPhase,
-//      snapshot.isMouseUserInteractionEnabled,
-//      let cursorDrawRun = snapshot.grid.drawRuns.cursorDrawRun,
-//      boundingRect.contains(cursorDrawRun.origin)
-//    {
-//      cursorDrawRun.draw(
-//        to: ctx,
-//        font: snapshot.font,
-//        appearance: snapshot.appearance,
-//        upsideDownTransform: snapshot.upsideDownTransform
-//      )
-//    }
+    guard let snapshot: DrawSnapshot = isolatedRenderContext.withLock({ renderContext in
+      guard
+        let renderContext,
+        let grid = renderContext.state.grids[gridID]
+      else {
+        return nil
+      }
+
+      let upsideDownTransform = CGAffineTransform(scaleX: 1, y: -1)
+        .translatedBy(x: 0, y: -Double(grid.rowsCount) * renderContext.state.font.cellHeight)
+
+      return DrawSnapshot(
+        grid: grid,
+        upsideDownTransform: upsideDownTransform,
+        font: renderContext.state.font,
+        appearance: renderContext.state.appearance,
+        cursorBlinkingPhase: renderContext.state.cursorBlinkingPhase,
+        isMouseUserInteractionEnabled: renderContext.state.isMouseUserInteractionEnabled
+      )
+    }) else {
+      return
+    }
+
+    ctx.saveGState()
+    defer { ctx.restoreGState() }
+
+    let boundingRect = IntegerRectangle(
+      frame: ctx.boundingBoxOfClipPath.applying(snapshot.upsideDownTransform),
+      cellSize: snapshot.font.cellSize
+    )
+
+    ctx.setAllowsAntialiasing(false)
+    ctx.setAllowsFontSmoothing(false)
+    ctx.setShouldAntialias(false)
+    ctx.setShouldSmoothFonts(false)
+    snapshot.grid.drawRuns.drawBackground(
+      to: ctx,
+      boundingRect: boundingRect,
+      font: snapshot.font,
+      appearance: snapshot.appearance,
+      upsideDownTransform: snapshot.upsideDownTransform
+    )
+
+    ctx.setAllowsAntialiasing(true)
+    ctx.setAllowsFontSmoothing(true)
+    ctx.setShouldAntialias(true)
+    ctx.setShouldSmoothFonts(true)
+    snapshot.grid.drawRuns.drawForeground(
+      to: ctx,
+      boundingRect: boundingRect,
+      font: snapshot.font,
+      appearance: snapshot.appearance,
+      upsideDownTransform: snapshot.upsideDownTransform
+    )
+
+    if
+      snapshot.cursorBlinkingPhase,
+      snapshot.isMouseUserInteractionEnabled,
+      let cursorDrawRun = snapshot.grid.drawRuns.cursorDrawRun,
+      boundingRect.contains(cursorDrawRun.origin)
+    {
+      cursorDrawRun.draw(
+        to: ctx,
+        font: snapshot.font,
+        appearance: snapshot.appearance,
+        upsideDownTransform: snapshot.upsideDownTransform
+      )
+    }
   }
 
   @MainActor
